c9208d6338268036876ed49c45f9a8691c808585
RishiTanu <singhrishikesh500@gmail.com>
2024-09-28 04:06:38 +0530

    initial commit -m practicing build types, varients, and build flavours and how we can exclude dependecies for specific build types, and how we can create a seperate module structure for code

---
+++ app/build.gradle.kts
@@ -0,0 +1,123 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.example.androidutilitiesnew"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.androidutilitiesnew"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    // Defining dimensions for flavors using the flavorDimensions property
+    flavorDimensions.addAll(listOf("version"))
+  //  flavorDimensions.addAll(listOf("version","environment"))
+    productFlavors{
+        create("free"){
+            dimension = "version"
+            applicationIdSuffix = ".free"
+            versionNameSuffix = "-free"
+            minSdk = 30
+        }
+        create("paid"){
+            dimension = "version"
+            applicationIdSuffix = ".paid"
+            versionNameSuffix = "-paid"
+            minSdk = 21
+        }
+        /*create("minSdk30"){
+            dimension = "environment"
+            applicationIdSuffix = ".minSdk30"
+            versionNameSuffix = "-minSdk30"
+            minSdk  = 30
+        }
+        create("minSdk21"){
+            dimension = "environment"
+            applicationIdSuffix = ".minSdk21"
+            versionNameSuffix = "-minSdk21"
+            minSdk  = 21
+        }*/
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        //build types
+        create("alpha"){
+            isMinifyEnabled =  false
+            applicationIdSuffix = ".alpha"
+            versionNameSuffix = "-alpha"
+        }
+
+        /*create("beta"){
+            isMinifyEnabled =  false
+            applicationIdSuffix = ".beta"
+            versionNameSuffix = "-beta"
+        }*/
+      /*  getByName("debug") {
+            isMinifyEnabled = false
+        }*/
+    }
+
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+        debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Exclude from all release variants (any flavor + release build type)
+    configurations.all {
+        if (name.contains("release", ignoreCase = true)) {
+            exclude(group = "androidx.ui", module = "ui-test-manifest")
+        }
+    }
+}
---
+++ app/src/debug/java/com/example/androidutilitiesnew/TimberDemo.kt
@@ -0,0 +1,4 @@
+package com.example.androidutilitiesnew
+
+class TimberDemo {
+}
